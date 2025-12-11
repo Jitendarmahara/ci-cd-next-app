e17ff4b1a542deb8deb394df6c711b898457aa28
Jitendar Mahara <jitendarmahara2002@gmail.com>
2025-12-12 05:00:13 +0530

    Push all pending files

--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -1,3 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg';
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL
+});
 
-export const client = new PrismaClient();
+const prismaClientSingleton = () => {
+    return new PrismaClient({
+    adapter: adapter
+    })
+}
+
+declare const globalThis: {
+    prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+
+export const client = prisma;
